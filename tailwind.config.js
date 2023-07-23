dc3e9040481e1bc9612c7dfafde3776f77b4d9d7
abubakar1702 <abubakar.akash17@gmail.com>
2023-07-23 23:47:19 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'press-start': ['"Press Start 2P"', 'cursive'],
+      }
+    },
+  },
+  plugins: [],
+}
+
